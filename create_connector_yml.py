ec0d76cb56ffeb6e7e500ec2e3b32b67fb5bed33
Rajesh Kalyanam <rkalyanapurdue@gmail.com>
2022-08-10 17:26:17 -0400

    initial commit of manifest and script

---
+++ create_connector_yml.py
@@ -0,0 +1,20 @@
+#!/bin/env python3
+
+import yaml
+import os
+
+connector_dict = {'$1': {'Input': {'Era5Input': 
+                                     {'dataset': os.environ['param_dataset'], 
+                                      'format': os.environ['param_format'], 
+                                      'region': os.environ['param_region'],
+                                      'origin': os.environ['param_origin'],
+                                      'variable': os.environ['param_variable'],
+                                      'time_aggregation': os.environ['param_time_aggregation'],
+                                      'horizontal_aggregation': os.environ['param_horizontal_aggregation'],
+                                      'year': os.environ['param_year'].split(','),
+                                      'version': os.environ['param_version'],
+                                      'uid':os.environ['param_uid'],
+                                      'api_key':os.environ['param_api_key']}}}}
+
+with open('/job/data/era5input.yml','w') as damfile:
+    yaml.dump(connector_dict,damfile)
---
+++ main.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/local/bin/run-workflow-stage.sh /job/data/era5input.yml 1:Input Input /job/result None None None None
